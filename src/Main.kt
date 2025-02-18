f60624dca33e4141f846383a9976d31dd2dfac63
Bashayer770 <Beshow95@gmail.com>
2025-02-18 17:57:28 +0300

    Bashayer

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,19 @@
 fun main() {
-    println("Hello World!")
+val age: Int = 50
+    println(age)
+var cityName: String? = "New York"
+    cityName = null
+    println(cityName)
+    var cityLength = cityName?.length ?: 0
+    println(cityLength)
+
+
+
+//    calculateScore(70.00)
+    println("Score 70, ${calculateScore(70.00)}")
+
+}
+fun calculateScore(score:Double?):String {
+    val finalScore = score?: 0.0
+    return  if(finalScore >= 50) "pass" else "fail"
 }
